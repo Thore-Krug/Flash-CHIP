2e80e05da99d771558536dc376264ff6ec713f00
Thore-Krug <36414231+Thore-Krug@users.noreply.github.com>
2019-04-18 22:24:26 +0200

    Update Flash.sh

--- Flash.sh
+++ Flash.sh
@@ -36,8 +36,6 @@ echo " Then press enter please "
 
 read flavour
 
-
-
 echo -e "\n Setting up environment"
 sudo apt-get -y update
 sudo apt-get install curl -y
@@ -50,21 +48,6 @@ sudo apt-get -y install \
  adb \
  u-boot-tools \
  android-tools-fastboot \
-
-echo -e "\n Adding current user to dialout group"
-sudo usermod -a -G dialout $(logname)
-
-echo -e "\n Adding current user to plugdev group"
-sudo usermod -a -G plugdev $(logname)
-
-
-echo -e "\n Adding udev rule for Allwinner device"
-echo -e 'SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="efe8", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="18d1", ATTRS{idProduct}=="1010", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip-fastboot"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="1010", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip-fastboot"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", GROUP="plugdev", MODE="0660" SYMLINK+="usb-serial-adapter"
-' | sudo tee /etc/udev/rules.d/99-allwinner.rules
-sudo udevadm control --reload-rules
 
 echo -e "\n Installing CHIP-tools"
 if [ -d CHIP-tools ]; then
